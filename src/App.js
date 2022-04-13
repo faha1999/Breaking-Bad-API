3dd34f825671d88b2d78f68320049a6fb4913888
faha1999 <kafaha1999@gmail.com>
2022-04-14 03:31:02 +0600

    applied useState & App State, useEffect & HTTP Request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './css/style.min.css';
 import React from 'react';
+import axios from 'axios';
 import Header from './components/ui/Header';
+import CharactersGrid from './components/characters/CharactersGrid';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`,
+      );
+
+      console.log(result.data);
+
+      setItems(result.data);
+      setIsLoading(false);
+    };
+    fetchItems();
+  }, []);
+
   return (
     <div className="container">
       <Header />
+      <CharactersGrid isLoading={isLoading} items={items} />
     </div>
   );
 }
---
+++ src/components/characters/CharactersGrid.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CharactersGrid = ({ items, isLoading }) => {
+  return <div>CharactersGrid</div>;
+};
+
+export default CharactersGrid;
