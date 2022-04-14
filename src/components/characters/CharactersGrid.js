eba10b9ddbb015e16969157ed65db74cb9101f0c
faha1999 <kafaha1999@gmail.com>
2022-04-15 01:56:58 +0600

    complete CharactersGrid

--- src/components/characters/CharactersGrid.js
+++ src/components/characters/CharactersGrid.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const CharactersGrid = ({ items, isLoading }) => {
-  return <div>CharactersGrid</div>;
+  return isLoading ? (
+    <h1>Loading...</h1>
+  ) : (
+    <section className="cards">
+      {items.map((item) => (
+        <h1 key={item.char_id}>{item.name}</h1>
+      ))}
+    </section>
+  );
 };
 
 export default CharactersGrid;
